1c52d91fc8b062aa09ffd29e462ef11e078c6927
Javid Ahmadi <jawid4mm@gmail.com>
2023-09-15 22:06:52 +0330

    Working on javascript file and finish that

--- app.js
+++ app.js
@@ -3,98 +3,67 @@ const itemContaner = document.querySelector(".list-item-contaner");
 
 const overlay = document.querySelector('.overlay');
 const closeForm = document.querySelector('#overlay-close');
-const save = document.querySelector(".save");
 const websiteName = document.querySelector('#website-name');
 const websiteLink = document.querySelector('#website-link');
+const form = document.querySelector('.form');
 
+let bookMarks = [];
 
-
-const bookMarks = [];
-
-
-
-
-
-
-function overlayRemove () {
+function addBookMark() {
     overlay.classList.toggle('hidden');
+    websiteName.focus();
 };
 
-// loading data form local storage when user came back to website
-function loadLocalStorageData() {
-    const localStorageData = JSON.parse(localStorage.getItem('bookMarks'));
-    console.log(localStorageData);
-
-    if (localStorageData){
-        localStorageData.forEach(value => {bookMarks.push(value)});
-
-        bookMarks.forEach(ee => {
-            const item = `
-                <div class="item">
-                    <img class="fav-icon" src="https://s2.googleusercontent.com/s2/favicons?domain=${ee.Wlink}" alt="${ee.Wname} icon"/>
-                    <a target="_blank" href="http://${ee.Wlink}">${ee.Wname}</a>
-                    <i class="remove" name="${ee.Wname}" class="remove">x</i>
-                </div> 
-            `;
-            itemContaner.insertAdjacentHTML('beforeend', item);
+function loadBookMarks () {
+    if (localStorage.getItem('bookMark')) {
+        bookMarks = JSON.parse(localStorage.getItem('bookMark'));
+        itemContaner.textContent = '';
+        bookMarks.forEach(el => {
+            const html = `
+            <div class="item">
+                <img class="fav-icon" src="https://s2.googleusercontent.com/s2/favicons?domain=${el.link}" alt="google icon"/>
+                <a href="${el.link}" target="_blank">${el.name}</a>
+                <i class="fas fa-times remove" adr="${el.link}" onclick="deleteBookMark('${el.link}')" title="Remove"></i>
+            </div> 
+        `;
+        itemContaner.insertAdjacentHTML('beforeend', html);
         });
-
-    }else{
-        console.log('there is nothing in local storage');
+    }
+    else {
+        console.log('there is no data in the local storage.');
     };
 };
 
-// save user's new book mark
-function saveInfo (e) {
+function saveBookMark (e) {
     e.preventDefault();
+    const nameValue = websiteName.value;
+    let linkValue = websiteLink.value;
+    linkValue = linkValue.includes("http://") && linkValue.includes("https://") ? linkValue : `http://${linkValue}`;
+
     bookMarks.push({
-        Wname : websiteName.value,
-        Wlink : websiteLink.value,
+        name : nameValue,
+        link : linkValue,
     });
 
-    localStorage.setItem('bookMarks', JSON.stringify(bookMarks));
-
-    const item = `
-        <div class="item">
-            <img class="fav-icon" src="https://s2.googleusercontent.com/s2/favicons?domain=${websiteLink.value}" alt="${websiteName.value} icon"/>
-            <a target="_blank" href="http://${websiteLink.value}">${websiteName.value}</a>
-            <i class="remove" name="${websiteName.value}">x</i>
-        </div> 
-    `;
-    itemContaner.insertAdjacentHTML('beforeend', item);
-    
-    overlayRemove();
-    websiteName.value = '';
-    websiteLink.value = '';
-    removeItemF();
+    localStorage.setItem('bookMark', JSON.stringify(bookMarks));
+    form.reset();
+    websiteName.focus();
+    loadBookMarks();
+    addBookMark();
 };
 
-// removing item form the bookmark
-function removeItemF() {
-    const removeItem = document.querySelectorAll('.remove')
-    removeItem.forEach(item => {
-        item.addEventListener('click', function () {
-            const e = JSON.parse(localStorage.getItem('bookMarks')).findIndex(value => {
-                return value.Wname == item.getAttribute('name');
-            });
-            console.log(e)
-            bookMarks.splice(e, 1)
-            console.log(bookMarks)
-            localStorage.setItem('bookMarks', JSON.stringify(bookMarks));
-            
-            loadLocalStorageData()
-        })
-    });
+// in the function bellow onclick is used. "li" is the value of onclick parameter, and you have to use this way.
+// if you try to remove any book mark the link of that will be copied as "li" value, which we are comparing with el.link.
+// if you dont understand this part just focus on onclick in line 26 and the relation of that with the function bellow.
+function deleteBookMark (li) {
+    bookMarks.splice(bookMarks.findIndex(el => el.link == li), 1);
+    localStorage.setItem('bookMark', JSON.stringify(bookMarks));
+    loadBookMarks();
 };
 
+// Event listener
+addList.addEventListener('click', addBookMark);
+closeForm.addEventListener('click', addBookMark);
+form.addEventListener('submit', saveBookMark);
 
-
-// Event Listener
-addList.addEventListener('click', overlayRemove);
-closeForm.addEventListener('click', overlayRemove);
-save.addEventListener('click', saveInfo);
-
-
-// onload functions
-loadLocalStorageData();
-removeItemF();
+loadBookMarks();
